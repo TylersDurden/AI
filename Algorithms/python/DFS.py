4141ea14371d7841e0b197505baef86eb2245d2b
TylersDurden <srobbin1@ithaca.edu>
2018-07-21 12:31:47 +0000

    Handling user input

--- Algorithms/python/DFS.py
+++ Algorithms/python/DFS.py
@@ -12,7 +12,8 @@ class DFS:
     rawdata = []
     xdim = -1
     ydim = -1
-    
+    valid_path = False
+
     def __init__(self, matrix, dims):
         self.rawdata = matrix
         self.xdim = dims[0]
@@ -39,21 +40,45 @@ class DFS:
                 continue
         np.array(matrix)
 
-
     def initialize(self, ptA, ptB):
-        x0 = ptA.pop(0)
         y0 = ptA.pop()
-        x1 = ptB.pop(0)
+        x0 = ptA.pop()
         y1 = ptB.pop()
-        print(self.rawdata.pop(x0).split(' ')[y0])
-        print(self.rawdata.pop(x1).split(' ')[y1])
+        x1 = ptB.pop()
+        xi = ""
+        yi = ""
+        xf = ""
+        yf = ""
+
+        if xi == 0:
+            xi = "0"
+        else:
+            xi = str(x0)
+        if yi == 0:
+            yi = "0"
+        else:
+            yi = str(y0)
+        if xf == 0:
+            xf = "0"
+        else:
+            xf = str(x1)
+        if yf == 0:
+            yf = "0"
+        else:
+            yf = str(y1)
         a = self.rawdata.pop(x0).split(' ')[y0]
         b = self.rawdata.pop(x1).split(' ')[y1]
-        if (a == '0' and b == '1') or (a == '1' and b == '0'):
-            print('')
+
+
+
+        if b == '1' and a == '1':
+            print(a+'->'+b)
+            print('Start point and endpoint are both ' + a + 's. Beginning a' +
+                  '[D]epth [F]irst [S]earch from:')
+            print('['+xi+','+yi+']->['+xf+','+yf+']')
+            self.valid_path = True
         else:
-            print('Start point and endpoint are both ' + a + 's. Beginning a'+
-                  '[D]epth [F]irst [S]earch')
+            print a+'->'+b+' is invalid!'
 
 
 def usage():
@@ -75,31 +100,33 @@ def get_DFSearchSpace():
     for item in start:
         ptA.append(item)
     for val in stop:
-        ptB.append(item)
-    return ptA, ptB
+        ptB.append(val)
+    pta = [ptA.__getitem__(0), ptA.__getitem__(1)]
+    ptb = [ptB.__getitem__(0), ptB.__getitem__(1)]
+    return pta, ptb
 
 
 def main():
     dims = []
     data = []
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         usage()
     else:
         if sys.argv[1] == '-matrix':
-            f = open(sys.argv[2],'rb')
+            f = open(sys.argv[2], 'rb')
             data = f.readlines()
         print(str(len(data))+'x'+str(len(data[0].split(' ')))+' input matrix.')
         dims.append(len(data[0].split(' ')))    # x-length
         dims.append(len(data))      # Y-length
 
-    # Initialize the Depth First Search Space
-    dfs = DFS(data, dims)
+        # Initialize the Depth First Search Space
+        dfs = DFS(data, dims)
 
-    # Define starting point and stopping point of the search
-    ptA, ptB = get_DFSearchSpace()
+        # Define starting point and stopping point of the search
+        ptA, ptB = get_DFSearchSpace()
 
-
-    dfs.initialize(ptA,ptB)
+        # Determine if endpoints are valid (must be equal)
+        dfs.initialize(ptA, ptB)
 
 
 if __name__ == '__main__':
