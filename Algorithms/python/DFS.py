28dfa6bd69ac1aa247b369cf9999545e231363ee
root <root@localhost.localdomain>
2018-07-21 11:29:55 +0000

    Graph creation and traversal

--- Algorithms/python/DFS.py
+++ Algorithms/python/DFS.py
@@ -15,38 +15,91 @@ class DFS:
     
     def __init__(self, matrix, dims):
         self.rawdata = matrix
-        self.xdim = dims(1)
-        self.ydim = dims(2)
+        self.xdim = dims[0]
+        self.ydim = dims[1]
     
     @staticmethod 
     def makeNxN(self):
-        
+        """
+
+        """
+        matrix = [[]]
+        Row = []
         row = 0 
         col = 0
         last_row = False
         for point in self.rawdata:
-            
+            Row.append(point)
             col += 1
-            if(col == self.xdim):
+            if col == self.xdim:
+                matrix[row] = Row
                 row += 1
                 col = 0
+                for element in Row: Row.remove(element)
+                continue
+        np.array(matrix)
+
+
+    def initialize(self, ptA, ptB):
+        x0 = ptA.pop(0)
+        y0 = ptA.pop()
+        x1 = ptB.pop(0)
+        y1 = ptB.pop()
+        print(self.rawdata.pop(x0).split(' ')[y0])
+        print(self.rawdata.pop(x1).split(' ')[y1])
+        a = self.rawdata.pop(x0).split(' ')[y0]
+        b = self.rawdata.pop(x1).split(' ')[y1]
+        if (a == '0' and b == '1') or (a == '1' and b == '0'):
+            print('')
+        else:
+            print('Start point and endpoint are both ' + a + 's. Beginning a'+
+                  '[D]epth [F]irst [S]earch')
 
 
 def usage():
     print('*--------<INCORRECT_USAGE>--------*')
-    print('* python  <flattened matrix>      *')
-    print('* flat mat format: use brackets,  *')
-    print('* and make it look like:          *')
-    print("*"+"\t"+str(np.array([1, 0, 0, 1, 1, 0]))+"\t\t  *")
+    print('* python  -type  <data.txt>       *')
     print('*---------------------------------*')
 
 
+def get_DFSearchSpace():
+    """
+    Let the user define the starting and stopping
+    points of the Depth First Search.
+    :return: Starting point ptA, and Endpoint ptB
+    """
+    start = input('Enter [x,y] coordinates of starting point: ')
+    stop = input('Enter [x,y] coordinates of stopping point: ')
+    ptA = []
+    ptB = []
+    for item in start:
+        ptA.append(item)
+    for val in stop:
+        ptB.append(item)
+    return ptA, ptB
+
+
 def main():
-    if len(sys.argv) < 3:
+    dims = []
+    data = []
+    if len(sys.argv) < 2:
         usage()
     else:
-        print(sys.argv[2].replace('[', ''))
+        if sys.argv[1] == '-matrix':
+            f = open(sys.argv[2],'rb')
+            data = f.readlines()
+        print(str(len(data))+'x'+str(len(data[0].split(' ')))+' input matrix.')
+        dims.append(len(data[0].split(' ')))    # x-length
+        dims.append(len(data))      # Y-length
 
+    # Initialize the Depth First Search Space
+    dfs = DFS(data, dims)
+
+    # Define starting point and stopping point of the search
+    ptA, ptB = get_DFSearchSpace()
+
+
+    dfs.initialize(ptA,ptB)
 
 
 if __name__ == '__main__':
--- Images/Automata.py
+++ Images/Automata.py
@@ -12,26 +12,71 @@ class AUTOMATA:
     Alphas = []
     bites = []
 
-    row = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    dot = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-    diag= [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    bkrow=[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    ones = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    zeta = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    diag = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
+    invr = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+    st3p = [[1, 1, 0], [1, 0, 0], [0, 1, 1]]
+    st0p = [[0, 0, 1], [0, 1, 1], [1, 1, 1]]
+    dots = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    xros = [[0, 1, 1], [1, 0, 1], [0, 0, 1]]
+
+    a = [[1, 0, 1], [0, 1 ,0], [0, 1, 0]]
+    b = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 0, 1, 0, 0]]
+    c = [[0, 0, 0], [0, 1, 1], [0, 0, 0]]
 
     def __init__(self):
         self.initializeIntegerMapping()
 
     def initializeIntegerMapping(self):
+        eye = np.concatenate((self.ones, self.dots, self.ones), 0)
+        i2i = np.concatenate((self.dots, self.ones, self.dots), 0)
+        stoop = np.concatenate((self.st0p, self.st3p, self.st0p), 0)
+        xros = np.concatenate((self.xros, self.dots, self.xros), 0)
+        mess = np.concatenate((xros, stoop), 1)
+        m3ss = np.concatenate((np.concatenate((xros,stoop),1),np.concatenate((xros,stoop),1)),0)
 
-        eye = np.concatenate((self.row,self.dot,self.row), 0)
-        invi = np.concatenate((self.bkrow, self.diag, self.bkrow), 0)
+        shapes = []
+        shapes.append(eye)
+        shapes.append(i2i)
+        shapes.append(stoop)
+        shapes.append(xros)
+        shapes.append(mess)
+        shapes.append(m3ss)
 
         plt.imshow(eye, 'gray')
+       # plt.show()
+        plt.imshow(i2i, 'gray')
+        plt.title('i2i')
+        #plt.show()
+        plt.imshow(stoop, 'gray')
+        #plt.show()
+        plt.title('st00p')
+        plt.imshow(xros,'gray')
         plt.show()
-        plt.imshow(invi, 'gray')
+        plt.title('xros')
+        plt.imshow(mess,'gray')
         plt.show()
+        plt.title('messy')
+        plt.imshow(m3ss,'gray')
+        plt.title('m3ss')
+        plt.plot()
+        plt.show()
+        print(m3ss.shape)
 
-        map = {}
-        #for int in np.arange(-255,255,1):
+
+    def stackSlabs(self, shapes):
+        allsame  = True
+        for shape in shapes:
+            for s in shapes:
+                if s != shape:
+                    False
+        if(allsame):
+            # Stack Em
+            for shape in shapes:
+                base = (np.concatenate((shapes[2], shapes[3]), 1), np.concatenate((shapes[2], shapes[3]), 1))
+        else:
+            return False
 
 def main():
     AUTOMATA()
